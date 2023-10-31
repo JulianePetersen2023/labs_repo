1a09c535f4c41d06bfec24152350cf0edfecf7e4
JulianePetersen2023 <146129216+JulianePetersen2023@users.noreply.github.com>
2023-10-31 18:16:32 +0100

    x

---
+++ LabsW3_SQL/startups.sql
@@ -0,0 +1,24 @@
+create database startups;
+
+use startups;
+
+create table startup (
+startup_id int not null unique auto_increment primary key,
+startup_name varchar (40),
+valuation int,
+date_founded varchar (40));
+
+create table industry (
+Industry_ID int not null unique auto_increment primary key,
+Industry_name varchar (40));
+
+create table city (
+city_id int not null unique auto_increment primary key,
+city_name varchar (40));
+
+create table country (
+country_id int not null unique auto_increment primary key,
+country_name varchar (40));
+
+
+
